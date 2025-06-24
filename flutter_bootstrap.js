e6697bf00e2aac8ac1d14064a87daa1d1aad24e3
ArnabDeepNath <ArnabDeepNath@users.noreply.github.com>
2025-06-24 15:53:54 +0000

    deploy: 619e5a448657716770f03417b555f0c2adc5b676

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,7 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1379262060",
-    timeoutMillis: 10000
+    serviceWorkerVersion: "2039016610"
   }
 });
